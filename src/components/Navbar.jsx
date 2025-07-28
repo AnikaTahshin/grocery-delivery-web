ab5479715068f5cedbea911da42d17e80be6334a
AnikaTahshin <anikatahsinmeem@gmail.com>
2025-07-28 20:02:57 +0600

    search functionality done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets"; // Adjust the path as necessary
  import { useAppContext } from "../context/AppContext";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-  const { user, setUser, setShowUserLogin, navigate,showUserLogin } = useAppContext();
-
+  const { user, setUser, setShowUserLogin, navigate,showUserLogin, searchQuery, setSearchQuery } = useAppContext();
   const logout = async () => {
     setUser(null);
     navigate("/"); 
   };
+
+  useEffect(() => {
+    if (searchQuery.length > 0) {
+        navigate('/products');
+        
+    }
+   
+  }, [searchQuery])
+  
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
@@ -25,6 +33,7 @@ const Navbar = () => {
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
           <input
+          onChange={(e) => setSearchQuery(e.target.value)}
             className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
             type="text"
             placeholder="Search products"
