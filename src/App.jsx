97e949d7c37f5a2055098ccf7903a85adb76ae82
AnikaTahshin <anikatahsinmeem@gmail.com>
2025-07-29 14:06:08 +0600

    cart details page done

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./components/Login";
 import AllProducts from "./pages/AllProducts";
 import ProductCategory from "./pages/ProductCategory";
 import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
 
 const AppContent = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -27,6 +28,8 @@ const AppContent = () => {
           <Route path="/products" element={<AllProducts />} />
           <Route path="/products/:category" element={<ProductCategory />} />
           <Route path="/products/:category/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+
 
 
         </Routes>
